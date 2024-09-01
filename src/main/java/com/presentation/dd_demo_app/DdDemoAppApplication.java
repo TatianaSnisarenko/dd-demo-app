e6d755e44cbd785e5dc47e75ad1147b01ee534b3
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-09-01 15:28:10 +0300

    initial commit

---
+++ src/main/java/com/presentation/dd_demo_app/DdDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.presentation.dd_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/presentation/dd_demo_app/DdDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presentation.dd_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
